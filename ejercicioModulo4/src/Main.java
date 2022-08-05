b4feabbd632bc6cf6b6da3ac911347725c034034
pedro9747 <99045129+pedro9747@users.noreply.github.com>
2022-08-05 18:54:09 -0300

    Add files via upload

--- ejercicioModulo4/src/Main.java
+++ ejercicioModulo4/src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Ejercicio if
+        int n = 0;
+        if (n > 0){
+            System.out.println("Es un número positivo");
+        }else{
+            if (n < 0){
+                System.out.println("Es un número negativo");
+            }else{
+                System.out.println("Es 0");
+            }
+        }
+
+        //Ejercicio while
+        int numeroWhile = 0;
+        while(numeroWhile<3){
+            System.out.println(numeroWhile);
+            numeroWhile ++;
+        }
+
+        //Ejercicio do while
+        int numerodoWhile = 5;
+        do {
+            System.out.println(numerodoWhile);
+            numerodoWhile ++;
+        }while (numerodoWhile<3);
+
+        //Ejercicio for
+        for(int numeroFor = 0;numeroFor <= 3 ;numeroFor++ ){
+            System.out.println(numeroFor);
+        }
+
+        //Ejercicio switch
+        var estacion = "VERANO";
+        switch(estacion) {
+            case ("VERANO"):
+            case ("PRIMAVERA"):
+            case ("OTOÑO"):
+            case ("INVIERNO"):
+                System.out.println("Estamos en " + estacion);
+                break;
+            default:
+                System.out.println("Eso no es una estación");
+
+        }
+
+
     }
+
+
 }
